ab5fee73b42784081f123633173d774d1dce257f
Agustin Medina <113214607+agustinelson@users.noreply.github.com>
2023-05-30 20:32:00 -0600

    Add files via upload

---
+++ src/interfaz/main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 13:32:54 2023
+
+@author: an.medinacolmenero
+"""
+"""
+import threading
+
+def run_file():
+    # Code to run your Python file
+    exec(open('buttons_Vpython.py').read())
+
+# Create a new thread
+thread = threading.Thread(target=run_file)
+
+# Start the thread
+thread.start()
+
+# Main thread continues executing other code
+
+# Wait for the thread to complete (optional)
+#thread.join()
+"""
+
+import subprocess
+import threading as td
+
+import threading
+
+def run_file():
+    # Code to run your Python file
+    subprocess.run(['python', 'buttons_Vpython.py'])
+
+# Create a new thread
+thread = threading.Thread(target=run_file)
+
+# Start the thread
+thread.start()
+
+# Main thread continues executing other code
+
+# Wait for the thread to complete (optional)
+#thread.join()
+
+
+print("end process")
